e1ec1180b6844c562954b8192402902884794b52
maciej.koziara <maciej.koziara@airhelp.com>
2021-06-17 14:27:11 +0200

    Init commit

--- src/main/java/pl/infoshare/aop/broken/BrokenService.java
+++ src/main/java/pl/infoshare/aop/broken/BrokenService.java
@@ -1,7 +1,6 @@
 package pl.infoshare.aop.broken;
 
 import org.springframework.stereotype.Component;
-import pl.infoshare.aop.retry.Retry;
 
 @Component
 public class BrokenService {
@@ -9,7 +8,6 @@ public class BrokenService {
     private final static int NUMBER_OF_EXCEPTIONS = 12;
     private int numberOfCalls = 0;
 
-    @Retry(value = BrokenException.class, maxAttempts = 15, delay = 1000)
     public void breakCode() {
         numberOfCalls++;
         if (numberOfCalls < NUMBER_OF_EXCEPTIONS) {
